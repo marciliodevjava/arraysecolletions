a040e28a0a4fdb507a0298cb32fd6511822f90b2
m-arc <m-arc@192.168.1.8>
2022-04-19 16:58:30 -0300

    Atualização

--- src/arrays/Foreach.java
+++ src/arrays/Foreach.java
@@ -7,11 +7,12 @@ public class Foreach {// utiliza para pecorrer um Array
 		double[] notas = { 9.9, 8.7, 7.2, 9.4 };
 
 		for (int i = 0; i < notas.length; i++) {
-			System.out.println(notas[i]);
+			System.out.println("Nota " + (i + 1) + ": " + notas[i]);
 		}
 
 		for (double nota : notas) {// Foreach para percorrer um array
-			System.out.println(nota);
+			System.out.println("Nota: " + nota);
+
 		}
 	}
 }
