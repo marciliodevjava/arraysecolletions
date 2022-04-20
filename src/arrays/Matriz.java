b57255c0a854fbd70490dce6e79602866afb01f0
m-arc <m-arc@192.168.1.8>
2022-04-20 08:52:56 -0300

    Atualização

--- src/arrays/Matriz.java
+++ src/arrays/Matriz.java
@@ -1,5 +1,6 @@
 package arrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Matriz {
@@ -10,7 +11,7 @@ public class Matriz {
 
 		System.out.println("Quantos alunos?");
 		int quantidadeDeAlunos = entrada.nextInt();
-		System.out.println("Qunatas notas de casa aluno?");
+		System.out.println("Qunatas notas por aluno?");
 		int quantidadeDeNotas = entrada.nextInt();
 		double[][] notasDaTurma = new double[quantidadeDeAlunos][quantidadeDeNotas];
 		int contadorAluno = 1;
@@ -20,7 +21,7 @@ public class Matriz {
 		for (int a = 0; a < notasDaTurma.length; a++) {
 			for (int n = 0; n < notasDaTurma.length; n++) {
 
-				System.out.printf("Informe a nota %d do aluno %d: ", (a + 1), (n + 1));
+				System.out.printf("Aluno %d nota %d: ", (a + 1), (n + 1));
 				String nt = entrada.next().replace(",", ".");
 				double notas = Double.parseDouble(nt);
 				notasDaTurma[a][n] = notas;
@@ -28,9 +29,13 @@ public class Matriz {
 			}
 		}
 		for (double[] ds : notasDaTurma) {
-			for (int i = 0; i < notasDaTurma.length; i++) {
+			for (int i = 0; i < notasDaTurma.length; i++) {// foreach para percorrer o arrays
 				System.out.println("Nota: " + ds[i]);
 			}
+		}
+
+		for (double[] notasDoAluno : notasDaTurma) {
+			System.out.println(Arrays.toString(notasDoAluno));// Para percorrer um arrays dentro da matriz
 		}
 
 		double media = total / (quantidadeDeAlunos * quantidadeDeNotas);
