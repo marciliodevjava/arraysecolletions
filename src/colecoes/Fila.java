a3c50ee9c169a47549be08d63f45e2b9b4197ddd
m-arc <m-arc@192.168.1.8>
2022-04-21 20:32:37 -0300

    Atualização

--- src/colecoes/Fila.java
+++ src/colecoes/Fila.java
@@ -19,8 +19,9 @@ public class Fila {
 		fila.offer("Gui");
 
 		// Peek e Element -> retornam o primeiro elemento da fila
-		// Diferença é que o peek ser não tiver retorna "null" e o elemente retorna uma
-		// exception.
+		// Diferença é que o peek ser não tiver nada na fila retorna "null" e o elemente
+		// retorna uma exception.
+		
 		System.out.println(fila.peek());
 		System.out.println(fila.peek());
 		System.out.println(fila.element());
