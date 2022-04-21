003a4ed106e35ebc04b5652ce59ef1e071c05176
m-arc <m-arc@192.168.1.8>
2022-04-21 20:53:53 -0300

    Pilha

---
+++ src/colecoes/Pila.java
@@ -0,0 +1,28 @@
+package colecoes;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Pila {
+
+	public static void main(String[] args) {
+
+		Deque<String> livros = new ArrayDeque<>();
+
+		livros.add("O Pequeno Príncipe");
+		livros.push("Don Quixote");
+		livros.push("O Hobbit");
+
+		System.out.println(livros.peek());
+		System.out.println(livros.element());
+
+		System.out.println(livros.poll());
+		System.out.println(livros.poll());
+		System.out.println(livros.poll());
+
+		// livros.size();
+		// livros.clear();
+		// livros.contains("O Hobbit");
+		// livros.isEmpty();
+	}
+}
