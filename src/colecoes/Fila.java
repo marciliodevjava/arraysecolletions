e8715ae1ffaf9d9eb928cdbe6769ef9528409590
m-arc <m-arc@192.168.1.8>
2022-04-21 20:43:16 -0300

    Atualização

--- src/colecoes/Fila.java
+++ src/colecoes/Fila.java
@@ -10,6 +10,7 @@ public class Fila {
 		Queue<String> fila = new LinkedList<>();
 
 		// Offer e Add -> adicionam elementos na fila
+
 		// Difrença é o comportamento quando a fila está cheia!
 		fila.add("Ana");// Retorna False
 		fila.offer("Bia");// Lança uma exception
@@ -19,11 +20,27 @@ public class Fila {
 		fila.offer("Gui");
 
 		// Peek e Element -> retornam o primeiro elemento da fila
-		// Diferença é que o peek ser não tiver nada na fila retorna "null" e o elemente
+
+		// Diferença é que o peek ser não tiver vazia na fila retorna "null" e o
+		// elemente
 		// retorna uma exception.
 
 		System.out.println(fila.peek());// Retorna null
 		System.out.println(fila.peek());
 		System.out.println(fila.element());// Lança uma exception
+		System.out.println(fila.element());
+
+		// fila.size() mostra o tamanho
+		// fila.clear() remove o elemento
+		// fila.isEmpty() verifica se a fila está cheia
+		// fila.pool() mostra o primeiro elemento e remove em seguida
+		// fila.remove() remove da lista
+
+		// Pool e remove -> obter o próximo elemento
+		// da fila e remove
+
+		System.out.println(fila.poll()); // retorna false
+		System.out.println(fila.remove());// Lança uma exception
+		System.out.println(fila.poll());
 	}
 }
