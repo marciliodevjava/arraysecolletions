8c1699afcd5fb5544f79068a7905e20a67e3a0f1
m-arc <m-arc@192.168.1.8>
2022-04-21 21:16:21 -0300

    Atualização

--- src/colecoes/Mapa.java
+++ src/colecoes/Mapa.java
@@ -2,6 +2,7 @@ package colecoes;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class Mapa {
 
@@ -15,8 +16,29 @@ public class Mapa {
 		usuarios.put(3, "Rafaela");
 		usuarios.put(4, "Rebeca");
 
-		System.out.println(usuarios.size());
-		System.out.println(usuarios.isEmpty());
-		System.out.println(usuarios.keySet());
+		System.out.println(usuarios.size());// mostra o tamanho
+		System.out.println(usuarios.isEmpty());// verifica se a fila está cheia
+		System.out.println(usuarios.keySet());// conjunto de chave
+		System.out.println(usuarios.values());// retorna valor
+		System.out.println(usuarios.entrySet());// retorna chave e valor
+		System.out.println(usuarios.containsKey(20));// verica se contens por chave
+		System.out.println(usuarios.containsValue("REbeca"));// verifica se contens por valor
+		System.out.println(usuarios.get(4));// pega a partir da chave retornando null
+
+		// lista por chave utilizando Foreach
+		for (int chave : usuarios.keySet()) {
+			System.out.println(chave);
+		}
+
+		// lista por valor utilizando Foreach
+		for (String valor : usuarios.values()) {
+			System.out.println(valor);
+		}
+
+		// lista por chave e valor utilizando Foreach
+		for (Entry<Integer, String> registro : usuarios.entrySet()) {
+			System.out.print(registro.getKey() + " ");
+			System.out.println(registro.getValue());
+		}
 	}
 }
